ad0466da946e4334d6b9c8975d2dba5dfbb3f5bd
Juan <juan.chavez@autozone.com>
2019-03-22 13:29:22 -0700

    One of the biggest changes in React 16 is how React handles JavaScript errors. It provides a built-in solution for handling errors gracefully, with a new lifecycle method called componentDidCatch().

--- src/App.js
+++ src/App.js
@@ -6,17 +6,30 @@ import StudentForm from './components/StudentForm';
 
 class App extends Component {
   
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+  state = {
+    hasError: false
+  };
 
-        <StudentForm />
-      </div>
-    );
+
+  componentDidCatch(){
+    this.setState({hasError: true});
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Oh no! Something went wrong.</h1>
+    } {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+  
+          <StudentForm />
+        </div>
+      );
+    }
   }
 }
 
